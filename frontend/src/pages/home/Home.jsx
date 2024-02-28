85471bda102be140d2f149cfdcb56031b31b7966
Younes Outerbah <younesouterbah1@gmail.com>
2024-02-28 21:54:33 +0100

    done with the project

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -20,6 +20,7 @@ export default function Home() {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           dispatch(setLocationAllowed(true));
+          setToggle(false);
         },
         () => {
           dispatch(setLocationAllowed(false));

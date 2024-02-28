32695d925f9d81203b120c9055b76ddda708db9c
Younes Outerbah <younesouterbah1@gmail.com>
2024-02-28 23:29:08 +0100

    deployment error

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,11 @@ import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import { error } from "./middlewares/error.js";
 import cors from "cors";
-import { path } from "path";
 
 connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);

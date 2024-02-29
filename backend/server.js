b4efab927e4fbd9892bf2533f93c191b86b3a82a
Eunice99X <110408536+YounesOuterbah@users.noreply.github.com>
2024-02-29 19:13:40 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,10 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use(error);
 app.use(
-  cors({
-    origin: ["https://grocery-store-intern-uj7c.vercel.app"],
-    credentials: true,
-  })
+    cors({
+        origin: "*",
+        credentials: true,
+    })
 );
 
 const PORT = process.env.PORT || 3344;

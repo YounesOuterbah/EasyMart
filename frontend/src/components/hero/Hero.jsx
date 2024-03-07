455a2684d1516a87a4510a1ec8c51f6d2879d357
Younes Outerbah <younesouterbah1@gmail.com>
2024-03-07 11:54:39 +0100

    linking subscribe function

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
 import heroBg from "/heroBg.png";
 import heroFront from "/heroFront.png";
 
 export default function Hero() {
+  const [email, setEmail] = useState("");
+
+  const handleInputChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const subscribe = async () => {
+    try {
+      const res = await fetch("https://grocerystoreintern.onrender.com/api/email/send-email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userEmail: email,
+          subject: "Subscription Confirmation",
+          htmlTemplate: "<p>Thank you for subscribing to Groceyish!</p>",
+        }),
+      });
+      if (response.ok) {
+        alert("Subscription successful!");
+      } else {
+        throw new Error("Error subscribing");
+      }
+    } catch (error) {
+      console.error("Error subscribing:", error);
+      alert("Error subscribing. Please try again later.");
+    }
+  };
+
   return (
     <div className="hero relative bg-[#c5ead9ab]">
       <img src={heroBg} className="h-full absolute top-0 left-0 object-cover opacity-90 -z-[2]" />
@@ -13,11 +44,15 @@ export default function Hero() {
           <p className="text-[--color-text-second] my-2">Save up to 60% off on your first order</p>
           <div className="flex m-auto md:m-0">
             <input
-              type="search"
+              type="email"
               className="border p-2 w-full md:w-[40%] outline-none bg-white"
+              onChange={handleInputChange}
+              value={email}
               placeholder="Enter your email address"
             />
-            <button className="bg-[--color-primary] text-white py-2 px-4">Subscribe</button>
+            <button onClick={subscribe} className="bg-[--color-primary] text-white py-2 px-4">
+              Subscribe
+            </button>
           </div>
         </div>
         <div>
